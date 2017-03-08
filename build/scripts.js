88e9d8548166269f08c93c3fdc7865cae34a63d5
Alexey Toklovich <alexey@toklovich.com>
2017-03-08 13:25:59 +0700

    Reports & slider fix

--- build/scripts.js
+++ build/scripts.js
@@ -11838,25 +11838,27 @@ $(document).ready(function () {
     $('[data-toggle]').toggler();
 });
 (function() {
-    var headerSlider = $('.js-header-slider').bxSlider({
-        infiniteLoop: true,
-        pager: false,
-        controls: false
-    });
+    $('.js-header-slider').each(function () {
+        var headerSlider = $(this).bxSlider({
+            infiniteLoop: true,
+            pager: false,
+            controls: false
+        });
 
-    $('.js-header-slider-next').click(function(){
-        headerSlider.goToNextSlide();
-        return false;
-    });
+        $('.js-header-slider-next').click(function(){
+            headerSlider.goToNextSlide();
+            return false;
+        });
 
-    $('.js-header-slider-prev').click(function(){
-        headerSlider.goToPrevSlide();
-        return false;
-    });
+        $('.js-header-slider-prev').click(function(){
+            headerSlider.goToPrevSlide();
+            return false;
+        });
 
-    $(window).resize(function(){
-        headerSlider.reloadSlider();
-    });
+        $(window).resize(function(){
+            headerSlider.reloadSlider();
+        });
+    })
 })();
 (function( $ ) {
     $.fn.toggler = function() {
