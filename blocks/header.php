e121117d86637a301e3c3430c406fe9058d3fdb7
alexeynechaev <30683342+alexeynechaev@users.noreply.github.com>
2017-08-04 15:37:46 +0700

    Add files via upload

--- blocks/header.php
+++ blocks/header.php
@@ -1,28 +1,38 @@
 <header class="main-header">
     <div class="wrapper">
         <a href="#" class="main-header-toggler js-offcanvas">
-            <svg xmlns="http://www.w3.org/2000/svg" width="21" height="16" viewBox="0 0 21 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 21 16">
                 <path fill="#48ABEC" d="M34,40H55v2.094H34V40Zm0,6.938H55v2.125H34V46.937Zm0,6.969H55V56H34V53.906Z" transform="translate(-34 -40)"></path>
             </svg>
         </a>
         <div class="main-header-logo">
             <a href="#">
-                <img src="images/logo.png" alt="Фонд профилактики рака. Живу не напрасно.">
+                <img height="60px;" src="images/logo.svg" alt="Фонд профилактики рака. Живу не напрасно.">
             </a>
         </div>
         <div class="main-header-right">
-            <div class="main-header-contacts">
+            <div class="main-header-contacts phone-col">
                 <div class="main-header-contacts-title">
                     Связаться с нами
                 </div>
                 <a href="#" class="main-header-contacts-phone">
+                    <img src="images/call-answer.svg">
                     +7 812 316-34-34
                 </a>
             </div>
+            <div class="main-header-contacts mail-col">
+                <a href="#" class="main-header-contacts-email">
+                    <img src="images/close-envelope.svg">
+                    fond@nenaprasno.ru
+                </a>
+            </div>
+
             <div class="main-header-buttons">
+            <!--
                 <div href="#" class="main-header-button-login">
                     <a href="#" class="link-dotted">Вход</a> / <a href="#" class="link-dotted">Регистрация</a>
                 </div>
+            -->
 
                 <div class="social-buttons">
                     <a href="#" target="_blank" title="Вконтакте">
@@ -36,12 +46,13 @@
                     </a>
                 </div>
             </div>
-        </div>
+        </div>  
     </div>
     <nav class="main-header-nav">
-        <div class="wrapper">
+        <div class="wrapper-nav">
 
             <ul class="main-header-nav-list">
+                <li><a href="#">Главная</a></li>
                 <li><a href="#">Фонд</a></li>
                 <li><a href="#">Проекты</a></li>
                 <li><a href="#">Как помочь?</a></li>
@@ -56,58 +67,3 @@
         </div>
     </nav>
 </header>
-
-<div id="offcanvas" class="main-offcanvas-overlay">
-    <nav class="main-offcanvas">
-        <div class="main-offcanvas-padding">
-            <div class="main-offcanvas-user">
-                <a href="#" class="link-dotted">Вход</a> / <a href="#" class="link-dotted">Регистрация</a>
-            </div>
-        </div>
-
-        <ul class="main-offcanvas-menu">
-            <li>
-                <a href="#">Фонд</a>
-            </li>
-            <li>
-                <a href="#">Проекты</a>
-            </li>
-            <li>
-                <a href="#">Как помочь?</a>
-            </li>
-            <li>
-                <a href="#">Публикации</a>
-            </li>
-            <li class="active">
-                <a href="#">Партнеры</a>
-
-                <ul>
-                    <li><a href="#">Доноры</a></li>
-                    <li><a href="#">Клиники и научные организации</a></li>
-                    <li><a href="#">Информационные партнеры</a></li>
-                    <li><a href="#">Инфраструктурные партнеры</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">Контакты</a>
-            </li>
-            <li>
-                <a href="#">Помочь прямо сейчас</a>
-            </li>
-        </ul>
-
-        <div class="main-offcanvas-padding">
-            <div class="social-buttons">
-                <a href="#" target="_blank" title="Вконтакте">
-                    <?php include "images/icon-social-vk.svg"; ?>
-                </a>
-                <a href="#" target="_blank" title="Facebook">
-                    <?php include "images/icon-social-facebook.svg"; ?>
-                </a>
-                <a href="#" target="_blank" title="Twitter">
-                    <?php include "images/icon-social-twitter.svg"; ?>
-                </a>
-            </div>
-        </div>
-    </nav>
-</div>
